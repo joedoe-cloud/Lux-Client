7b062eac302b02506cb98adbf2dc6ed35291253f
joedoe-cloud <i98w3b3613@hello.kuromee.store>
2026-07-22 17:25:08 +0200

    Create LuxClient.java

---
+++ src/main/java/de/luxclient/LuxClient.java
@@ -0,0 +1,56 @@
+package de.luxclient;
+
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Main entry point of Lux Client.
+ *
+ * This class is loaded by Fabric when Minecraft starts.
+ *
+ * Future systems:
+ * - ModuleManager
+ * - ConfigManager
+ * - ClickGUI
+ * - EventBus
+ */
+public class LuxClient implements ClientModInitializer {
+
+
+    public static final String NAME = "Lux Client";
+
+    public static final String VERSION = "v1";
+
+
+    public static final Logger LOGGER =
+            LoggerFactory.getLogger("LuxClient");
+
+
+
+    @Override
+    public void onInitializeClient() {
+
+
+        LOGGER.info(
+                "{} {} initialized.",
+                NAME,
+                VERSION
+        );
+
+
+        /*
+         * Future initialization:
+
+           ModuleManager.init();
+
+           ConfigManager.load();
+
+           EventManager.register();
+
+         */
+
+    }
+}
