dba0472352a168d92ae119706965cd6e64fc1f39
joedoe-cloud <i98w3b3613@hello.kuromee.store>
2026-07-23 15:19:41 +0200

    Create Keystrokes.java

---
+++ src/main/java/de/luxclient/module/modules/visual/Keystrokes.java
@@ -0,0 +1,43 @@
+package de.luxclient.module.modules.visual;
+
+import de.luxclient.module.Category;
+import de.luxclient.module.Module;
+
+public class Keystrokes extends Module {
+
+
+    public Keystrokes() {
+
+        super(
+                "Keystrokes",
+                "Displays movement keys and mouse buttons",
+                Category.VISUAL
+        );
+
+    }
+
+
+    @Override
+    public void onEnable() {
+
+        /*
+         * Später:
+         *
+         * Keystrokes Renderer aktivieren
+         */
+
+    }
+
+
+    @Override
+    public void onDisable() {
+
+        /*
+         * Später:
+         *
+         * Renderer deaktivieren
+         */
+
+    }
+
+}
